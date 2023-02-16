73ba47bde42ede58e571bd9ac6516a0033ca2e4f
sagarklti <sagark.patra@lntinfotech.com>
2023-02-16 03:33:22 -0800

    profile updation

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import WithAuthentication from "../../helper/components/with-authentication";
+import { connect } from "react-redux";
+import { mapStateToProps } from '../../store/Selectors/commonSelectors'
 
-const Profile = () => {
+const Profile = (props) => {
+  console.log(props)
   return (
     <div>
       <h1>Profile page</h1>
+     <p> User:{props.userName}</p>
+     <p> Role:{props.role}</p>
+      <p>SignedIn?:{props.isSignedIn?'yes':'no'}</p>
+
     </div>
   );
 };
 
-export default WithAuthentication(Profile);
+export default connect(mapStateToProps)(WithAuthentication(Profile));
